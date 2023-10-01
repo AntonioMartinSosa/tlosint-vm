46681ffc8dd8dde4b188c8e7c60824673225e4b0
Tom Hocker <tom@humandecoded.io>
2023-10-01 09:58:27 -0400

    added keepassxc and desktop icon

--- scripts/tl/install-obsidian.sh
+++ scripts/tl/install-obsidian.sh
@@ -4,3 +4,5 @@
 wget -O /home/osint/Obsidian.AppImage https://github.com/obsidianmd/obsidian-releases/releases/download/v1.4.14/Obsidian-1.4.14.AppImage 
 chmod +x /home/osint/Obsidian.AppImage
 chmod +x /home/osint/Desktop/obsidian.desktop
+sudo apt install keepassxc
+chmod +x /home/osint/keepass.desktop
